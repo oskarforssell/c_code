4ce6325a1b3a8f9e2915636266d11e294eace5ee
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-05 10:33:18 +0200

    cleaned up

--- 05-Arrays/FirstHomework/ArrayGrower/Program.cs
+++ 05-Arrays/FirstHomework/ArrayGrower/Program.cs
@@ -6,19 +6,25 @@ namespace ArrayGrower
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give a number (negative number quits):");
-            
-            int givenNumber = 0;                            // neede to starts the while loop
-            string[] numbers = new string[0];               // create an Array with 0-slots
-            int entries = 0;                                // calculate number of user entries 
-            while (givenNumber > -1) {                      // while user number is zero or positive -> loop continues
-                entries++;                                  // count the entries
-                string userInput = Console.ReadLine();      // get user input
-                Array.Resize(ref numbers, entries);         // resize Array to be as big as number of entries
-                givenNumber = int.Parse(userInput);         // convert user input (string) to integer
-                numbers[entries-1] = userInput;             // assign user input to Array using entries (-1 to get correct slot)
+            Console.WriteLine("Give a number (negative number quits):");  // instructions
+
+            int givenNumber = 0;                          // needed to starts the while loop
+            string[] numbers = new string[0];             // create an Array with 0-slots
+            int entries = 0;                              // calculate number of user entries 
+
+            while (givenNumber > -1)                      // while user number is 0 or positive -> loop continues
+            {                      
+                entries++;                                // count the entries
+                string userInput = Console.ReadLine();    // get user input
+                givenNumber = int.Parse(userInput);       // convert user input (string) to integer
+                if (givenNumber < 0)                      // avoid storing negative number in Array
+                {
+                    break;
+                }
+                Array.Resize(ref numbers, entries);       // resize Array to be as big as number of entries
+                numbers[entries - 1] = userInput;         // assign user input to Array using entries (-1 to get correct slot)
             }
-            Console.WriteLine(string.Join(',',numbers));    // join the arrays together and print 'em out \o/
+            Console.WriteLine(string.Join(',', numbers)); // join the arrays together and print 'em out
         }
     }
 }
