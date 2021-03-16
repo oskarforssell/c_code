0cf99e06e87e21477845e8af10b91d397a37fc69
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-16 09:57:41 +0200

    first class commit

---
+++ 06-Class/FirstClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.Greet("Oskar");
+
+            MyClass dod = new MyClass();
+            dod.Greet("table");
+
+        }
+    }
+
+    class MyClass
+    {
+        public void Greet(string str)
+        {
+            Console.WriteLine("Hello " + str);
+        }
+    }
+}
