45ed0787c167baa92ba755144b6833d005d69ffa
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-17 16:51:45 +0200

    enemy added

--- 07-oop-classes/ClassesHomework/Program.cs
+++ 07-oop-classes/ClassesHomework/Program.cs
@@ -28,6 +28,14 @@ namespace ClassesHomework
             Console.WriteLine($"My superhero name: {boy.HeroName}, superpower: {boy.HeroAbility}");
             Console.WriteLine($"I'm: {boy.GetAge} sun rotations old and human: {boy.HumanOrNot}");
 
+            Console.WriteLine("-----\nTHE ENEMY");
+            Enemy tim = new Enemy();
+            Console.WriteLine($"Name: {tim.Name}, HP: {tim.HealtPoints}, Alive: {tim.Alive}");
+            tim.Hit(5);
+            Console.WriteLine($"Name: {tim.Name}, HP: {tim.HealtPoints}, Alive: {tim.Alive}");
+            tim.Hit(96);
+            Console.WriteLine($"Name: {tim.Name}, HP: {tim.HealtPoints}, Alive: {tim.Alive}");
+
         }
     }
 }
