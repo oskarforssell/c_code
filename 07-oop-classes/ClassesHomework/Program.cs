4b90cec40b5d54c0ca6d0923283ec766300061af
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-17 08:41:02 +0200

    chair part done

--- 07-oop-classes/ClassesHomework/Program.cs
+++ 07-oop-classes/ClassesHomework/Program.cs
@@ -6,7 +6,20 @@ namespace ClassesHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the 'chair experience'!");
+
+            Chair officeChair = new Chair();
+            Console.WriteLine($"Our {officeChair.Color} office chair has {officeChair.Legs} legs");
+            Console.WriteLine($"Is someone sitting on it now: {officeChair.IsSitting}");
+            officeChair.Legs = 2;
+            officeChair.Color = "red";
+            officeChair.IsSitting = true;
+            Console.WriteLine($"Cut some legs to have {officeChair.Legs} legs, and paint it to make it {officeChair.Color}.");
+            Console.WriteLine($"Is someone sitting on it now: {officeChair.IsSitting}");
+            Console.WriteLine($"But it can only handle: {officeChair.MaxWeight}");
+
+
+    
         }
     }
 }
