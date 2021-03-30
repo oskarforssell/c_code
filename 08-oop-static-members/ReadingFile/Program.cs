1b14291a78703b06065c9658d7b9e63293591ca0
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-30 10:03:44 +0300

    file location moved

---
+++ 08-oop-static-members/ReadingFile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("./data.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+
+                    Console.WriteLine(line);
+                    break;
+                }
+            }
+        }
+    }
+    class DataReader
+    {
+        public static string Name(string row)
+        {
+            string[] divided = row.Split(",");
+            string age = divided[1];
+            foreach (char c in age)
+            {
+                if (Char.IsNumber(c))
+                {
+                    
+                    return "hi";
+                }
+            
+            }
+        }
+
+    }
+
+}
