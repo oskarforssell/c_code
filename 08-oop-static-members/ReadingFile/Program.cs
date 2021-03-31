b0d901790bdf2df87e6a26396a49d9e21b4e9f0e
oskarforssell <oskar.forssell@student.samk.fi>
2021-03-31 15:03:51 +0300

    updated

--- 08-oop-static-members/ReadingFile/Program.cs
+++ 08-oop-static-members/ReadingFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ReadingFile
 {
@@ -7,36 +8,37 @@ namespace ReadingFile
     {
         static void Main(string[] args)
         {
+            List<Person> peeps = new List<Person>();
+
             using (var reader = new StreamReader("./data.csv"))
             {
+                int line_num = 0;
                 while (!reader.EndOfStream)
                 {
+                    if (line_num == 0)
+                    {
+                        line_num++;
+                        reader.ReadLine();
+                        continue;
+                    }
                     string line = reader.ReadLine();
+                    //Console.WriteLine(line);
 
+                    var values = line.Split(",");
 
-                    Console.WriteLine(line);
-                    break;
+                    //Console.WriteLine(values[0] + "------" + values[1]);
+
+                    string name = values[0];
+                    int age = Int16.Parse(values[1]);
+
+                    Person p = new Person(name, age);
+                    p.Print();
+
+                    peeps.Add(p);
+                    //break;
                 }
             }
+            Console.WriteLine("peeps count:" + peeps.Count);
         }
     }
-    class DataReader
-    {
-        public static string Name(string row)
-        {
-            string[] divided = row.Split(",");
-            string age = divided[1];
-            foreach (char c in age)
-            {
-                if (Char.IsNumber(c))
-                {
-                    
-                    return "hi";
-                }
-            
-            }
-        }
-
-    }
-
 }
