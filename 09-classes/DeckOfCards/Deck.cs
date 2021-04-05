742620a110fee4c9193ebf0ba3010a31057b26a1
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-05 14:38:10 +0300

    completed

--- 09-classes/DeckOfCards/Deck.cs
+++ 09-classes/DeckOfCards/Deck.cs
@@ -6,48 +6,80 @@ namespace DeckOfCards
     class Deck
     {
         public List<Card> cards = new List<Card>();
-       
-        int[] rankNumbers = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13};
-        int[] suitNumbers = new int[] {1,2,3,4};
+
+        int[] rankNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        int[] suitNumbers = new int[] { 1, 2, 3, 4 };
 
         public Deck()
         {
-            for (int i=0; i<4; i++) {
-                for (int a=0; a<13; a++) {
-                    Card kort = new Card(a,i);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int a = 0; a < 13; a++)
+                {
+                    Card kort = new Card(a, i);
                     cards.Add(kort);
                     //kort.Print();
                 }
             }
             int deck_size = cards.Count;
         }
-        public void Print() {
+        public void PrintDeckSize()
+        {
             int deck_size = cards.Count;
-            Console.WriteLine(deck_size);
-
+            Console.WriteLine($"The deck has {deck_size} cards");
         }
-        
+        public void PrintDeckCards()
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                Card print_card = cards[i];
+                print_card.Print();
+            }
+        }
 
+        public void DrawRandomCards(int numberOfCardsToDraw)
+        {
+            if (numberOfCardsToDraw <= cards.Count)
+            {
+                for (int i = 0; i < numberOfCardsToDraw; i++)
+                {
+                    int deck_indexes = (cards.Count - 1);
+                    Random r = new Random();
+                    int randIndex = r.Next(0, deck_indexes);
+                    Card chosen_card = cards[randIndex];
+                    chosen_card.Print();
+                    cards.RemoveAt(randIndex);
+                }
+            }
+            else { Console.WriteLine($"Can't take {numberOfCardsToDraw} cards, only {cards.Count} cards left.."); }
+        }
 
+        public void DrawFromTopOfDeck(int cardsToBeDrawn)
+        {
+            if (cardsToBeDrawn <= cards.Count)
+            {
+                for (int i = 0; i < cardsToBeDrawn; i++)
+                {
+                    Card chosen_card = cards[0];
+                    chosen_card.Print();
+                    cards.RemoveAt(0);
+                }
+            }
+            else { Console.WriteLine($"Can't take {cardsToBeDrawn} cards, only {cards.Count} cards left.."); }
+        }
 
-        
-
-
-
-        // list of suits (spade, diamond, heart, club)
-        // list of ranks (1-13)
-        // list of cards (13*spade, 13*diamond, 13*heart, 13*club = 52 cards)
-        
-        // represent the deck here
-        // collection of 13 ranks each of 4 suits
-
-        // print out the number of cards in the deck
-
-        // PUBLIC METHODS
-        // drawing any number of random cards from the deck (limited to number of cards lef in deck)
-        // drawing any number of cards in order (limited to number of cards left in deck)
-        // shuffle the deck
-
-
+        public void ShuffleTheDeck()
+        {
+            for (int n = 0; n < 25; n++)
+            {
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    Random rand = new Random();
+                    int randomTake = rand.Next(0, (cards.Count - 1));
+                    cards.Add(cards[randomTake]);
+                    cards.RemoveAt(randomTake);
+                }
+            }
+        }
     }
 }
--- 09-classes/DeckOfCards/Program.cs
+++ 09-classes/DeckOfCards/Program.cs
@@ -8,15 +8,27 @@ namespace DeckOfCards
         static void Main(string[] args)
         {
             Deck pino = new Deck();
-            Card kortti = new Card(1,1);
-            kortti.Print();
+            //Card kortti = new Card(1,1);
+            //kortti.Print();
             //pino.cards.Add(kortti);
             
             // Card kortti = new Card(2,"spade");
             // pino.cards.Add(kortti);
-            pino.Print();
+            pino.PrintDeckSize();
             
+            // pino.DrawRandomCards(47);
+            // pino.Print();
+            // pino.DrawRandomCards(8);
+            // pino.Print();
+            //pino.DrawFromTopOfDeck(5);
+            //pino.Print();
+            pino.PrintDeckCards();
+            pino.ShuffleTheDeck();
+            pino.PrintDeckSize();
             
+            pino.PrintDeckCards();
+
+
             
             
         }
