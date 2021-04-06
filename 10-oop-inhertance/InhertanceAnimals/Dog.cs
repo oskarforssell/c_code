c68d12046b414d31b5278fa58613b63e9e545244
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-06 12:00:59 +0300

    dog class inherit from animal

---
+++ 10-oop-inhertance/InhertanceAnimals/Dog.cs
@@ -0,0 +1,22 @@
+namespace InhertanceAnimals
+{
+    public abstract class Dog : Animal
+    {
+
+        public string Name {get; set;} = "No name set!";
+        public Dog()
+        {
+            this.EyeCount = 2;
+            this.LegCount = 4;
+        }
+
+        public override void MakeSound()
+        {
+            System.Console.WriteLine("Woof woof!");
+            // base.MakeSound();
+        }
+
+        public abstract void Bark(); 
+        
+    }
+}
