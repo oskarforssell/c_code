570a061dfeb012d080e5feda1ea8193d9c421a86
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-20 11:22:08 +0300

    interface

---
+++ InterfaceInventory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inventroy = []
+            List<IInventoryItem> inventory = new List<IInventoryItem>();
+
+            // insert weapon
+            Weapon w1 = new Weapon("Excalibur", "sword");
+            inventory.Add(w1);
+
+            // insert health potion
+            HealthPotion h1 = new HealthPotion("Great HP POTION", 100);
+            inventory.Add(h1);
+
+            // inventory[0].Name
+            foreach (var item in inventory)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            // use the itams in the inventory
+            inventory[0].Use();
+            inventory[1].Use();
+        }
+    }
+}
