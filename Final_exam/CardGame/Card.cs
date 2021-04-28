cf9353a3e0c24222d97a779b3e449eec49799d4d
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-28 09:14:36 +0300

    card class

---
+++ Final_exam/CardGame/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CardGame
+{
+    class Card
+    {
+        public string[] suitArray = new string[] {"spade", "diamond", "heart", "club"};
+        public int[] rankArray = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+
+        public int Rank { get; private set; }
+        public string Suit { get; private set; }
+        
+        public Card(int rank, int suit)
+        {
+            //Random rnd = new Random();
+            //Rank = rnd.Next(1,13);
+            Rank = rankArray[rank];
+            Suit = suitArray[suit];
+        }
+        public void Print() {
+            Console.WriteLine($"{Rank} of {Suit}s");
+        }
+    }
+}
