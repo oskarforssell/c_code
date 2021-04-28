359a1f9b7840787ffda5d4aa6bcf41642214ce67
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-28 09:14:13 +0300

    start updated

--- Final_exam/CardGame/Program.cs
+++ Final_exam/CardGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CardGame
 {
@@ -6,7 +7,45 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("How many cards do you want (1-26)? ");
+                string ans = Console.ReadLine();
+                int digit = Int16.Parse(ans);
+                if (digit == 0) { break; }
+                else if (digit > 26)
+                {
+                    Console.WriteLine("Too manny cards! You can have 26 at most!");
+                }
+                else if (digit < 27)
+                {
+                    GameEngine newGame = new GameEngine(digit);
+                    break;
+                }
+            }
+
+            // Console.WriteLine(ans);
+
+            // create
+
+            // newGame.ToString();
+
+
+            // Deck pino = new Deck();
+            // pino.PrintDeckSize();
+            // Console.WriteLine("------------");
+            // pino.PrintDeckCards();
+            // Console.WriteLine("------------");
+
+            // pino.ShuffleTheDeck();
+            // pino.PrintDeckSize();
+            // Console.WriteLine("------------");
+            // pino.PrintDeckCards();
+            // Console.WriteLine("-----Random cards-------");
+            // pino.DrawRandomCards(3);
+
+
+
         }
     }
 }
