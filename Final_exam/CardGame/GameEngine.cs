bfb400727a753839e70eb95da6866fea160404bf
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-29 21:43:59 +0300

    refined

--- Final_exam/CardGame/GameEngine.cs
+++ Final_exam/CardGame/GameEngine.cs
@@ -15,40 +15,32 @@ namespace CardGame
                 int digit = Int16.Parse(ans);
                 if (digit == 0)
                 {
-                    Console.WriteLine("Well you quit early!");
+                    Console.WriteLine("0 means ZERO cards.. so no game! You loose!");
                     break;
                 }
                 else if (digit > 26)
                 {
                     Console.WriteLine("Too manny cards! You can have 26 at most!");
                 }
-                else if (digit < 1)
+                else if (digit < 0)
                 {
-                    Console.WriteLine("Insanity! You can't play without cards!! 0__0");
+                    Console.WriteLine("Insanity! You can't play with a negative ammount of cards!! 0__0");
                 }
                 else if (digit < 27)
                 {
-                    Console.WriteLine($"The size of the hands: {ans}");
-                    GameEngine newGame = new GameEngine(digit);
+                    Hand hand = new Hand();
+                    Console.WriteLine($"The size of the hands: {ans}\n");
+                    hand.Deal(digit);
+
+                    int playOn = 1;
+                    while (playOn != 0)
+                    {
+                        playOn = hand.PlayCard();
+                    }
                     break;
                 }
             }
         }
         public GameEngine() { }
-
-        public GameEngine(int cardsPerPlayer)
-        {
-            Hand hand = new Hand();
-            hand.GetPlayerHand(cardsPerPlayer);
-
-            int playOn = 1;
-            while (playOn != 0)
-            {
-                playOn = hand.PlayCard();
-            }
-
-        }
-
-        // based on how many cards the player wants, take that many random cards and give it to player and AI. (so between 1-26 cards)
     }
 }
