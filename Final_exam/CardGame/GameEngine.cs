b0e7673a3b373971f5544b46b856646cae8b68bb
oskarforssell <oskar.forssell@student.samk.fi>
2021-04-30 14:35:52 +0300

    methods from hand class

--- Final_exam/CardGame/GameEngine.cs
+++ Final_exam/CardGame/GameEngine.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 namespace CardGame
 {
     public class GameEngine
     {
-        // Deck pack = new Deck();
         Hand hand = new Hand();
         public void Start()
         {
@@ -27,7 +25,7 @@ namespace CardGame
                     // playWin[1] = 0 -> player plays first / 1 -> ai plays first
                     while (playWin[0] != 0)
                     {
-                        playWin = hand.PlayCard(playWin[1]);
+                        playWin = PlayCard(playWin[1]);
                     }
                     break;
                 }
@@ -52,6 +50,174 @@ namespace CardGame
                 hand.pack.cards.RemoveAt(0);
             }
             hand.pack.PrintDeckSize();
+        }
+
+        public int[] PlayCard(int playingOrder)
+        {
+            int[] continueGameAndRoundWinner = new int[2];
+
+            if (playingOrder == 0)
+            {
+                int[] output = PlayerFirst(playingOrder);
+                return output;
+            }
+            else
+            {
+                int[] output = AiFirst(playingOrder);
+                return output;
+            }
+        }
+        public int[] PlayerFirst(int playingOrder)
+        {
+            GamePlayQuestion();
+            int[] output = new int[2];
+            while (true)
+            {
+                int ans = Int32.Parse(Console.ReadLine());
+                if (ans <= hand.PlayerHandSize() - 1 && ans > -1)
+                {
+                    int[] playerCard = Player(ans);
+                    int[] aiCard = AI();
+                    output[1] = Winner(playerCard[0], aiCard[0], playerCard[1], aiCard[1], playingOrder);
+
+                    if (hand.PlayerHandSize() == 0)
+                    {
+                        output[0] = 0;
+                        return output;
+                    }
+                    output[0] = 1;
+                    return output;
+                }
+                else { Console.WriteLine($"Hey dumdum! You have to choose between (0-{(hand.PlayerHandSize() - 1).ToString()})"); }
+            }
+        }
+        public int[] AiFirst(int playingOrder)
+        {
+            int[] output = new int[2];
+            while (true)
+            {
+                int[] aiCard = AI();
+                GamePlayQuestion();
+                int ans = Int32.Parse(Console.ReadLine());
+                if (ans <= hand.PlayerHandSize() - 1 && ans > -1)
+                {
+                    int[] playerCard = Player(ans);
+                    output[1] = Winner(playerCard[0], aiCard[0], playerCard[1], aiCard[1], playingOrder);
+
+                    if (hand.PlayerHandSize() == 0)
+                    {
+                        output[0] = 0;
+                        return output;
+                    }
+                    output[0] = 1;
+                    return output;
+                }
+                else { Console.WriteLine($"Hey dumdum! You have to choose between (0-{(hand.PlayerHandSize() - 1).ToString()})"); }
+            }
+        }
+        public void GamePlayQuestion()
+        {
+            string cardRange = (hand.PlayerHandSize() - 1).ToString();
+            if (hand.PlayerHandSize() > 1)
+            {
+                hand.PrintPlayerHand();
+                Console.Write($"Which card do you want to play? (0-{cardRange}): ");
+            }
+            else if (hand.PlayerHandSize() == 1)
+            {
+                hand.PrintPlayerHand();
+                Console.Write($"Last card to play! (0): ");
+            }
+        }
+
+        public int[] Player(int ans)
+        {
+            Card oneCard = hand.playerHand[ans];
+            Console.Write("The player chose ");
+            oneCard.Print();
+            string playedCard = oneCard.ReturnCard();
+            int[] suitRank = new int[2] { oneCard.CardSuit(), oneCard.CardRank() };
+            hand.playerHand.RemoveAt(ans);
+            return suitRank;
+        }
+        public int[] AI()
+        {
+            Random random = new Random();
+            int selection = random.Next(0, (hand.AIHand.Count - 1));
+            Card aiCard = hand.AIHand[selection];
+            Console.Write("The AI chose ");
+            aiCard.Print();
+            int[] suitRank = new int[2] { aiCard.CardSuit(), aiCard.CardRank() };
+            hand.AIHand.RemoveAt(selection);
+            return suitRank;
+        }
+
+        public int Winner(int playerSuit, int aiSuit, int playerRank, int aiRank, int playingOrder)
+        {
+            if (playingOrder == 1) // Means AI played first
+            {
+                if (playerSuit == aiSuit)
+                {
+                    if (playerRank < aiRank)
+                    {
+                        AiWon();
+                        return 1;
+                    }
+                    else
+                    {
+                        PlayerWon();
+                        return 0;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Player picked a card with a different suit! One point for the AI!\n");
+                    hand.aiScore++;
+                    Scoreboard();
+                    return 1;
+                }
+            }
+            else
+            {
+                if (playerSuit == aiSuit)
+                {
+                    if (playerRank > aiRank)
+                    {
+                        PlayerWon();
+                        return 0;
+                    }
+                    else
+                    {
+                        AiWon();
+                        return 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("AI picked a card with a different suit! One point for the player!\n");
+                    hand.playerScore++;
+                    Scoreboard();
+                    return 0;
+                }
+            }
+            void PlayerWon()
+            {
+                Console.WriteLine("Player scored one point!\n");
+                hand.playerScore++;
+                Scoreboard();
+            }
+            void AiWon()
+            {
+                Console.WriteLine("AI scored one point!\n");
+                hand.aiScore++;
+                Scoreboard();
+            }
+        }
+        public void Scoreboard()
+        {
+            Console.WriteLine("-SCOREBOARD-");
+            Console.WriteLine($" Player: {hand.playerScore.ToString()}\n AI:\t {hand.aiScore.ToString()}");
+            Console.WriteLine("------------------------------------\n");
         }
     }
 }
